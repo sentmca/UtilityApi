9142a713d6997150f647eec0443eab8fcb89f80a
SenthilMurugan <sentmca@gmail.com>
2012-09-20 11:03:49 +0530

    Update src/util/FileUtils.java
    added readFileasString method

--- src/util/FileUtils.java
+++ src/util/FileUtils.java
@@ -1,4 +1,24 @@
 import java.io.*;
 class FileUtils{
+  
+   private static String readFileAsString(String filePath)
+    throws java.io.IOException{
+        StringBuffer fileData = new StringBuffer(1000);
+        BufferedReader reader = new BufferedReader(
+                new FileReader(filePath));
+        char[] buf = new char[1024];
+        int numRead=0;
+        while((numRead=reader.read(buf)) != -1){
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+            buf = new char[1024];
+        }
+        reader.close();
+        return fileData.toString();
+    }
+
 
 }
+
+
+
